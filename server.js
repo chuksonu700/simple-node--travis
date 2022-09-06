4e192cca0827017605f1d3ab2472dcf16b3ee50b
chuksonu700 <chuksonu700@gmail.com>
2022-09-06 14:49:28 +0100

    'assigment'

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const FAV_TEACHER = process.env.NAME;
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const express = require('express');
+const path = require("path");
 
-async function main() {
-  while(true) {
-    console.log("Microservices rock! ",FAV_TEACHER);
-    await sleep(5000);
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname, '/501.html'));
+})
+
+
+app.get('/health',(req,res)=>{
+
+  let ranNum = Math.floor(Math.random()*100000000000000000000+1);
+  res.send(`I AM VERY HEALTHY 0 ${ranNum}`);
+})
+
+const port = process.env.PORT || 4300
+
+app.listen(port,()=>{
+  console.log(`Server running on http://localhost:${port}`)
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
-}
-
-main();
+  
+  async function main() {
+    while(true) {
+      console.log("Microservices rock! ");
+      await sleep(5000);
+    }
+  }
+  
+  main();
+  
+})
