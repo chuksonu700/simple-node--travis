f4e945ade7742bb789301297243a6e35496d7a6d
chuksonu700 <chuksonu700@gmail.com>
2022-09-06 17:22:24 +0100

    thanks

--- server.js
+++ server.js
@@ -1,34 +1,11 @@
-const express = require('express');
-const path = require("path");
-
+const express = require("express");
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname, '/501.html'));
-})
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
 
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello!");
+});
 
-app.get('/health',(req,res)=>{
-
-  let ranNum = Math.floor(Math.random()*100000000000000000000+1);
-  res.send(`I AM VERY HEALTHY 0 ${ranNum}`);
-})
-
-const port = process.env.PORT || 4040
-
-app.listen(port,()=>{
-  console.log(`Server running on http://localhost:${port}`)
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-  async function main() {
-    while(true) {
-      console.log("Microservices rock! ");
-      await sleep(5000);
-    }
-  }
-  
-  main();
-  
-})
